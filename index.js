b88b634624afa1021c83958743381e5fe27e9b67
Heeji Hyun <wlgmlgus@gmail.com>
2022-07-15 13:02:24 -0400

    commit tree 4f68666098e4f9587f1ba36b3bc197f33a788376

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply)
+
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 8;
+let num4 = 36;
+let mod = num4 % num3;
+console.log(mod)
+
+
+let max = Math.max(1,2,3,4,20)
